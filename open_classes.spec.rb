ed11fabc0b3449ce500cdc3a11d4b18322dee8e1
Mark Haskamp <mrhask@gmail.com>
2010-07-11 10:11:16 -0400

    more putzing with before :all in open_classes

--- open_classes.spec.rb
+++ open_classes.spec.rb
@@ -2,26 +2,26 @@ require 'spec'
 require 'spec/autorun'
 
 describe "Open Classes" do
-  class String
-    def yo
-      'whatitdo!'
-    end
-  end
-
-  class Numeric
-    def minutes
-      self * 60
-    end
-  end
-
   before :all do
-
-    it "'yo' returns string 'whatitdo!'" do
-      "foo".yo.should eql 'whatitdo!'
+    class String
+      def yo
+        'whatitdo!'
+      end
     end
 
-    it "'minutes' multiplies self by 60" do
-      5.minutes.should eql 300
+    class Numeric
+      def minutes
+        self * 60
+      end
     end
   end
+
+  it "'yo' returns string 'whatitdo!'" do
+    "foo".yo.should eql 'whatitdo!'
+  end
+
+  it "'minutes' multiplies self by 60" do
+    5.minutes.should eql 300
+  end
+
 end
