e41def25ea67b3f4ad115c72ac96310ab9b755e1
Mark Haskamp <mrhask@gmail.com>
2010-07-11 09:31:42 -0400

    rspec adjustments so the tests work as expected when I run all the tests

---
+++ numeric.spec.rb
@@ -0,0 +1,22 @@
+require 'spec'
+require 'spec/autorun'
+
+describe Numeric do
+  describe "methods" do
+    it "'abs' returns the absolute value of a number" do
+      9.abs.should eql(9)
+      -9.abs.should eql(9)
+    end
+
+    it "'zero?' returns true if number == 0" do
+      (9 - (3*3)).zero?.should be_true
+
+      j = 0
+      j.zero?.should be_true
+    end
+  end
+
+  it "'minutes' should raise NoMethodError" do
+    lambda { 5.minutes }.should raise_error(NoMethodError)
+  end
+end
--- open_classes.spec.rb
+++ open_classes.spec.rb
@@ -1,25 +1,27 @@
 require 'spec'
 require 'spec/autorun'
 
-class String
-  def yo
-    'whatitdo!'
-  end
-end
-
-class Fixnum
-  def minutes
-    self * 60
-  end
-end
-
 describe "Open Classes" do
-
-  it "'yo' returns string 'whatitdo!'" do
-    "foo".yo.should eql 'whatitdo!'
+  class String
+    def yo
+      'whatitdo!'
+    end
   end
 
-  it "'minutes' multiplies self by 60" do
-    5.minutes.should eql 300
+  class Numeric
+    def minutes
+      self * 60
+    end
+  end
+
+  before :all do
+
+    it "'yo' returns string 'whatitdo!'" do
+      "foo".yo.should eql 'whatitdo!'
+    end
+
+    it "'minutes' multiplies self by 60" do
+      5.minutes.should eql 300
+    end
   end
 end
