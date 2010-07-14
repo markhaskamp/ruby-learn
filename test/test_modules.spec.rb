d92d2f717f0c4b5cea070e68ccf2ca28a0e3058d
Mark Haskamp <mrhask@gmail.com>
2010-07-13 23:50:00 -0400

    if include a module

--- test/test_modules.spec.rb
+++ test/test_modules.spec.rb
@@ -82,10 +82,59 @@ describe "Modules" do
 
       time1 = Time.now.to_i  # seconds since the epoch
       5.seconds.ago.to_i.should be_close(time1 - 5, 1)
-      1.hours.ago.to_i.should be_close(time1 - 3600, 2)
+      1.hours.ago.to_i.should be_close(time1 - 3600, 1)
+    end
+  end
+
+  describe "mixin with Missiles" do
+    before :all do
+
+      module Missile
+        def launch
+          "fire!"
+        end
+      end
+
+      module Missile_Mock
+        def launch
+          "let's don't and say we did"
+        end
+      end
+
+      class Missile_Commands
+        def self.set_command s
+          if s == :forreal
+            include Missile
+          else
+            include Missile_Mock
+          end
+        end
+        def get_target
+          "getting target"
+        end
+      end
+    end
+
+    it "should get a target" do
+      m = Missile_Commands.new
+      m.get_target.should eql "getting target"
+    end
+
+    it "launch for real" do
+      Missile_Commands.set_command :forreal
+      m = Missile_Commands.new
+      m.launch.should eql "fire!"
+    end
+
+    it "launch a mock" do
+      Missile_Commands.set_command :mock
+      m = Missile_Commands.new
+      m.launch.should eql "let's don't and say we did"
     end
 
   end
+      
+
 end
 
 
